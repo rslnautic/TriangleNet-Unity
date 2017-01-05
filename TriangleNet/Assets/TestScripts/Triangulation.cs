a5a5e76eb26459a6fd621198f40e9c1c50b57f67
Ramón Serrano López <rslnautic@gmail.com>
2017-01-06 00:54:37 +0100

    Cross shape mesh generated with Triangle.Net

---
+++ TriangleNet/Assets/TestScripts/Triangulation.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TriangleNet.Geometry;
+using UnityEngine;
+
+public class Triangulation {
+
+    public static bool triangulate(List<Vector2> points, List<List<Vector2>> holes, out List<int> outIndices, out List<Vector3> outVertices)
+    {
+        outVertices = new List<Vector3>();
+        outIndices = new List<int>();
+        Polygon poly = new Polygon();
+
+        for(int i = 0; i < points.Count; i++)
+        {
+            poly.Add(new Vertex(points[i].x, points[i].y));
+
+            if (i == points.Count - 1)
+            {
+                poly.Add(new Segment(new Vertex(points[i].x, points[i].y), new Vertex(points[0].x, points[0].y)));
+            } else {
+                poly.Add(new Segment(new Vertex(points[i].x, points[i].y), new Vertex(points[i + 1].x, points[i + 1].y)));
+            }
+        }
+
+        // Holes
+        for (int i = 0; i < holes.Count; i++)
+        {
+            List<Vertex> vertices = new List<Vertex>();
+            for (int j = 0; j < holes[i].Count; j++)
+            {
+                vertices.Add(new Vertex(holes[i][j].x, holes[i][j].y));
+            }
+            poly.Add(new Contour(vertices), true);
+        }
+
+        var mesh = poly.Triangulate();
+
+        foreach (ITriangle t in mesh.Triangles)
+        {
+            for(int j = 2; j>=0; j--)
+            {
+                bool found = false;
+                for (int k = 0; k < outVertices.Count; k++)
+                {
+                    if ((outVertices[k].x == t.GetVertex(j).X) && (outVertices[k].z == t.GetVertex(j).Y))
+                    {
+                        outIndices.Add(k);
+                        found = true;
+                        break;
+                    }
+                }
+
+                if(!found)
+                {
+                    outVertices.Add(new Vector3((float) t.GetVertex(j).X, 0, (float)t.GetVertex(j).Y));
+                    outIndices.Add(outVertices.Count - 1);
+                }
+            }
+        }
+        return true;
+    }
+}
---
+++ TriangleNet/Assets/TestScripts/TriangulatorTest.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriangulatorTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject go = new GameObject();
+        go.name = "Cross";
+        MeshFilter mf = go.AddComponent<MeshFilter>();
+        go.AddComponent<MeshCollider>();
+        go.AddComponent<MeshRenderer>();
+
+        Mesh mesh = mf.mesh;
+
+        List<Vector2> points = new List<Vector2>();
+        List<List<Vector2>> holes = new List<List<Vector2>>();
+        List<int> indices = null;
+        List<Vector3> vertices = null;
+
+        points.Add(new Vector2(10, 0));
+        points.Add(new Vector2(20, 0));
+        points.Add(new Vector2(20, 10));
+        points.Add(new Vector2(30, 10));
+        points.Add(new Vector2(30, 20));
+        points.Add(new Vector2(20, 20));
+        points.Add(new Vector2(20, 30));
+        points.Add(new Vector2(10, 30));
+        points.Add(new Vector2(10, 20));
+        points.Add(new Vector2(0, 20));
+        points.Add(new Vector2(0, 10));
+        points.Add(new Vector2(10, 10));
+
+        List<Vector2> hole = new List<Vector2>();
+        hole.Add(new Vector2(12, 12));
+        hole.Add(new Vector2(18, 12));
+        hole.Add(new Vector2(18, 18));
+        hole.Add(new Vector2(12, 18));
+
+        holes.Add(hole);
+
+        Triangulation.triangulate(points, holes, out indices, out vertices);
+
+        mesh.Clear();
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = indices.ToArray();
+        //mesh.Optimize();
+        mesh.RecalculateNormals();
+
+        go.GetComponent<MeshCollider>().sharedMesh = mesh;
+
+        Vector2[] uvs = new Vector2[mesh.vertices.Length];
+        for(int i = 0; i < uvs.Length; i++)
+        {
+            uvs[i] = new Vector2(mesh.vertices[i].x, mesh.vertices[i].y);
+        }
+        mesh.uv = uvs;
+    }
+	
+	void Update () {
+		
+	}
+}
